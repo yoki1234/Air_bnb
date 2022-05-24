dae044629635f5791ca6f21b314fbe7dbb8f7892
Akshay Ashok <akshayashokdj@gmail.com>
2022-05-24 18:07:15 +0530

    Air-bnb

--- Air_bnb/myscript.js
+++ Air_bnb/myscript.js
@@ -5,3 +5,23 @@ var swiper = new Swiper(".mySwiper", {
     dynamicBullets: true,
   },
 });
+// var lastScrolltop=0;
+//     inscreen_footer= document.getElementsByClassName("ina");
+//   window.addEventListener("scroll",()=>{let scrollTop = window.pageYOffset||Document
+//     .documentElement.scrollTop;
+//     if(scrollTop < lastScrolltop){
+//       inscreen_footer.style.bottom="-114px";
+
+//     }
+//     else{
+//       inscreen_footer.style.bottom="0";
+//     }
+//     lastScrolltop = scrollTop;
+//   })
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 0) {
+      $('.ina').hide();
+  } else {
+      $('.ina').show();
+  }
+});
